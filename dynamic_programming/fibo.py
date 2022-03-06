5033375ad9ed02dae4cf8526192a21ee024b105f
jiyongs <shzy232@naver.com>
2022-03-06 19:43:44 +0900

    edit fibo.py

--- dynamic_programming/fibo.py
+++ dynamic_programming/fibo.py
@@ -1,12 +1,12 @@
 # 피보나치 수열 (재귀적)
 
-# 그냥 재귀 > fibo(100)에도 수백억년 소요됨...
+# 재귀 > fibo(100)에도 수백억년 소요됨... : O(2^N)
 def fibo(x):
     if x == 1 or x == 2:
         return 1
     return fibo(x-1)+fibo(x-2)
 
-# 메모이제이션 활용 > 0.1초 걸림
+# 메모이제이션 + 재귀 (탑다운 방식) > 0.1초 걸림 : O(N)
 d = [0] * 100
 
 def fibo(x):
@@ -17,4 +17,15 @@ def fibo(x):
     d[x] = fibo(x-1)+fibo(x-2)
     return d[x]
 
+# 반복문 (버텀업 방식)
+d = [0] * 100
+def fibo(x):
+    d[1] = 1
+    d[2] = 1
+    for i in range(3, x+1):
+        d[i] = d[i-1]+d[i-2]
+    return d[x]
+
 print(fibo(99))
+
+# (참고) 시스템상 재귀함수의 스택 크기가 한정되어 있을 수 있다. sys의 setrecurtionlimit() 함수로 제한을 완화할 수 있다.
